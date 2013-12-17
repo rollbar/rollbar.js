f37147976bc1a689c874290f652c00875310e6e7
sbezboro <sergei.b.ru@gmail.com>
2013-12-16 17:17:58 -0800

    add util.js to grunt

--- dist/rollbar.js
+++ dist/rollbar.js
@@ -1,5 +1,8 @@
 (function(window, document){
 function Notifier(parentNotifier) {
+  this.payloadQueue = [];
+  this.plugins = {};
+
   if (parentNotifier) {
     // If the parent notifier has the shimQueue
     // property it means that it's the Rollbar shim.
@@ -15,8 +18,6 @@ function Notifier(parentNotifier) {
       this.configure(parentNotifier.options);
     }
   }
-  this.payloadQueue = [];
-  this.plugins = {};
 }
 
 Notifier._generateLogFn = function(level) {
@@ -124,6 +125,75 @@ Notifier.prototype.scope = function(options) {
   scopedNotifier.configure(scopedOptions);
 
   return scopedNotifier;
+};
+;var Util = {
+  merge: function(to, from) {
+    var k;
+    for (k in from) {
+      if (from[k].constructor == Object) {
+        if (!to.hasOwnProperty(k)) {
+          to[k] = from[k];
+        } else {
+          to[k] = merge(to[k], from[k]);
+        }
+      } else {
+        to[k] = from[k];
+      }
+    }
+    return to;
+  },
+
+  copy: function(obj) {
+    var dest = {};
+    Util.merge(dest, obj);
+    return dest;
+  },
+
+  parseUriOptions: {
+    strictMode: false,
+    key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+    q:   {
+      name:   "queryKey",
+      parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+    },
+    parser: {
+      strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+      loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+    }
+  },
+
+  parseUri: function(str) {
+    var o = Util.parseUriOptions;
+    var m = o.parser[o.strictMode ? "strict" : "loose"].exec(str);
+    var uri = {};
+    var i = 14;
+
+    while (i--) {
+      uri[o.key[i]] = m[i] || "";
+    }
+
+    uri[o.q.name] = {};
+    uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+      if ($1) {
+        uri[o.q.name][$1] = $2;
+      }
+    });
+
+    return uri;
+  },
+
+  sanitizeUrl: function(url) {
+    if (url) {
+      var baseUrlParts = Util.parseUri(url);
+      // remove a trailing # if there is no anchor
+      if (baseUrlParts.anchor === '') {
+        baseUrlParts.source = baseUrlParts.source.replace('#', '');
+      }
+      var baseUrl = baseUrlParts.source.replace('?' + baseUrlParts.query, ''); 
+      url = baseUrl;
+    }
+    return url;
+  }
 };
 ;if (!window._rollbarInitialized) {
   var shim = window.Rollbar;
