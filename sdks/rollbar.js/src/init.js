cd57f5a3899eb28f2ac036af41dd792bc2f5ea10
Cory Virok <coryvirok@gmail.com>
2013-12-17 10:37:16 -0800

    added util to grunt + basic implementation of processShimQueue @brianr @sbezboro

--- sdks/rollbar.js/src/init.js
+++ sdks/rollbar.js/src/init.js
@@ -1,6 +1,7 @@
 if (!window._rollbarInitialized) {
   var shim = window.Rollbar;
   var fullRollbar = new Notifier(shim);
+  fullRollbar._processShimQueue(window.RollbarShimQueue || []);
   window.Rollbar = fullRollbar;
   window._rollbarInitialized = true;
 }
