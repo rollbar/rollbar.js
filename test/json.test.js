879dd0fe09519089cab0cd42f8e7e8e090f2825a
sbezboro <sergei.b.ru@gmail.com>
2013-12-18 16:19:39 -0800

    rework custom stringify to handle cyclic references, more json tests @coryvirok

--- test/json.test.js
+++ test/json.test.js
@@ -4,7 +4,33 @@ describe('RollbarJSON', function() {
   it("should generate json string successfully using custom stringify", function(done) {
     var stringify = RollbarJSON.setupCustomStringify();
     var json = stringify({a: {b: 'c', d: 1}, e: true});
+
     expect(json).to.equal('{"a":{"b":"c","d":1},"e":true}');
+
+    done();
+  });
+
+  it("should handle bad input", function(done) {
+    var stringify = RollbarJSON.setupCustomStringify();
+    var object = {a: {b: 'c'}};
+    object.self = object;
+
+    // Make sure circular references are caught
+    expect(function() {
+      stringify(object);
+    }).to.throw(TypeError, 'RollbarJSON.stringify cannot serialize cyclic structures.');
+
+    expect(function() {
+      stringify(window);
+    }).to.throw(TypeError, 'RollbarJSON.stringify cannot serialize cyclic structures.');
+
+    expect(function() {
+      stringify(document.querySelector('#mocha'));
+    }).to.throw(TypeError, 'RollbarJSON.stringify cannot serialize cyclic structures.');
+
+    expect(stringify()).to.equal(undefined);
+    expect(stringify(null)).to.equal('null');
+
     done();
   });
 });
