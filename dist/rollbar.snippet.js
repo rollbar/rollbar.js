9ad9ebfca8d16e03143b142e103a12bc1193cc3a
Cory Virok <coryvirok@gmail.com>
2013-12-29 00:27:46 -0800

    readme for snippet changes + sauce labs integration for running mocha tests on their VMs. It doesn't work very well right now... probably something to do with the timeouts we use in the tests. @brianr

--- dist/rollbar.snippet.js
+++ dist/rollbar.snippet.js
@@ -119,12 +119,7 @@ for (var i = 0; i < _methods.length; ++i) {
   Rollbar.prototype[_methods[i]] = stub(_methods[i]);
 }
 
-var config = {
-  accessToken: 'ACCESS_TOKEN',
-  captureUncaught: true
-};
-
 var defaultRollbarJsUrl = '//d37gvrvc0wt4s1.cloudfront.net/js/v1.0/rollbar.min.js';
-var r = Rollbar.init(window, config);
+var r = Rollbar.init(window, _rollbaConfig);
 r.loadFull(window, document, true, defaultRollbarJsUrl);
 })(window, document);
--- src/shimload.js
+++ src/shimload.js
@@ -1,8 +1,3 @@
-var config = {
-  accessToken: 'ACCESS_TOKEN',
-  captureUncaught: true
-};
-
 var defaultRollbarJsUrl = '//d37gvrvc0wt4s1.cloudfront.net/js/v1.0/rollbar.min.js';
-var r = Rollbar.init(window, config);
+var r = Rollbar.init(window, _rollbaConfig);
 r.loadFull(window, document, true, defaultRollbarJsUrl);
