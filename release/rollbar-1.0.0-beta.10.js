fd3ec2fed97eefbadb2c413d2adb2fdf8b4fb406
Cory Virok <coryvirok@gmail.com>
2014-03-07 15:21:45 -0800

    bump and release to 1.0.0-rc.1 @sbezboro

--- release/rollbar-1.0.0-beta.10.js
+++ release/rollbar-1.0.0-beta.10.js
@@ -1538,7 +1538,7 @@ var XHR = {
 
 
 // Updated by the build process to match package.json
-Notifier.NOTIFIER_VERSION = '1.0.0-rc.1';
+Notifier.NOTIFIER_VERSION = '1.0.0-beta.10';
 Notifier.DEFAULT_ENDPOINT = 'api.rollbar.com/api/1/';
 Notifier.DEFAULT_SCRUB_FIELDS = ["passwd","password","secret","confirm_password","password_confirmation"];
 Notifier.DEFAULT_LOG_LEVEL = 'debug';
