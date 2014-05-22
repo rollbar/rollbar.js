b11192b750f3a2fa5fce1cba038bd53d91d4df3d
Cory Virok <coryvirok@gmail.com>
2014-05-21 18:18:25 -0700

    fix bug that created/used global variable

--- sdks/rollbar.js/src/notifier.js
+++ sdks/rollbar.js/src/notifier.js
@@ -1,6 +1,6 @@
 
 // Updated by the build process to match package.json
-Notifier.NOTIFIER_VERSION = '1.0.0-rc.7';
+Notifier.NOTIFIER_VERSION = '1.0.0-rc.8';
 Notifier.DEFAULT_ENDPOINT = 'api.rollbar.com/api/1/';
 Notifier.DEFAULT_SCRUB_FIELDS = ["passwd","password","secret","confirm_password","password_confirmation"];
 Notifier.DEFAULT_LOG_LEVEL = 'debug';
@@ -402,6 +402,7 @@ Notifier.prototype._getBrowserPlugins = function() {
     var cur;
     var numPlugins = navPlugins.length;
     var plugins = [];
+    var i;
     for (i = 0; i < numPlugins; ++i) {
       cur = navPlugins[i];
       plugins.push({name: cur.name, description: cur.description});
