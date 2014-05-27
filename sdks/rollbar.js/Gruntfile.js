2dcd976e19d6ddf91243d94c15ddbccea3f78890
Cory Virok <coryvirok@gmail.com>
2014-05-27 15:25:06 -0700

    minifying required js

--- sdks/rollbar.js/Gruntfile.js
+++ sdks/rollbar.js/Gruntfile.js
@@ -40,6 +40,7 @@ module.exports = function(grunt) {
         files: {
           'dist/<%= pkg.name %>.min.js': 'dist/<%= pkg.name %>.js',
           'dist/<%= pkg.name %>.snippet.min.js': 'dist/<%= pkg.name %>.snippet.js',
+          'dist/<%= pkg.name %>.require.min.js': 'dist/<%= pkg.name %>.require.js',
           'dist/<%= pkg.name %>.shim.min.js': 'src/shim.js',
           'dist/plugins/jquery.min.js': 'src/plugins/jquery.js'
         }
