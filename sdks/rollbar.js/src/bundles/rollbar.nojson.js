e82b6708d07f7bdc14dd933885d34c0437032e58
Jon de Andres <jon@wuaki.tv>
2015-05-07 15:29:10 +0200

    Build browser distribution (no json) with webpack.

---
+++ sdks/rollbar.js/src/bundles/rollbar.nojson.js
@@ -0,0 +1,19 @@
+var notifier = require('../notifier');
+var Notifier = notifier.Notifier;
+
+notifier.init(JSON);
+
+if (!window._rollbarInitialized) {
+  var config = window._rollbarConfig || {};
+  var alias = config.globalAlias || 'Rollbar';
+  var shim = window[alias];
+  var fullRollbar = new Notifier(shim);
+
+  fullRollbar._processShimQueue(window._rollbarShimQueue || []);
+  window[alias] = fullRollbar;
+  window._rollbarInitialized = true;
+
+  Notifier.processPayloads();
+}
+
+Rollbar.configure(_rollbarConfig)
