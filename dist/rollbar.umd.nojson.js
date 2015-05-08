23fcfffff9ab7869bdc07ca9784f879c66079a9d
Jon de Andres <jon@wuaki.tv>
2015-05-08 13:07:50 +0200

    Move webpack outputs to the correct folder.

--- dist/rollbar.umd.nojson.js
+++ dist/rollbar.umd.nojson.js
@@ -54,8 +54,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var notifier = __webpack_require__(1);
-	var Notifier = notifier.Notifier;
+	var globalnotifier = __webpack_require__(2);
 	
 	function setupJSON() {
 	  var JSONObject = JSON;
@@ -67,27 +66,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	    var customJSON = {};
 	    setupCustomJSON(customJSON);
+	
 	    JSONObject = customJSON;
 	  }
 	
-	  notifier.setupJSON(JSONObject);
+	  globalnotifier.setupJSON(JSONObject);
 	}
 	
 	setupJSON();
 	
-	if (!window._rollbarInitialized) {
-	  var config = window._rollbarConfig || {};
-	  var alias = config.globalAlias || 'Rollbar';
-	  var shim = window[alias];
-	  var fullRollbar = new Notifier(shim);
-	
-	  fullRollbar._processShimQueue(window._rollbarShimQueue || []);
-	  window._rollbarInitialized = true;
-	
-	  Notifier.processPayloads();
-	}
-	
-	module.exports = fullRollbar;
+	module.exports = globalnotifier.wrapper;
 
 
 /***/ },
@@ -1037,7 +1025,92 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 2 */,
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var notifier = __webpack_require__(1);
+	
+	var Notifier = notifier.Notifier;
+	// Stub out the wrapped error which is set 
+	window._rollbarWrappedError = null;
+	
+	function setupJSON(JSON) {
+	  notifier.setupJSON(JSON);
+	}
+	
+	// Global window.onerror handler
+	function _rollbarWindowOnError(client, old, args) {
+	  if (!args[4] && window._rollbarWrappedError) {
+	    args[4] = window._rollbarWrappedError;
+	    window._rollbarWrappedError = null;
+	  }
+	
+	  client.uncaughtError.apply(client, args);
+	  if (old) {
+	    old.apply(window, args);
+	  }
+	}
+	
+	function _extendListenerPrototype(client, prototype) {
+	  if (prototype.hasOwnProperty && prototype.hasOwnProperty('addEventListener')) {
+	    var oldAddEventListener = prototype.addEventListener;
+	    prototype.addEventListener = function(event, callback, bubble) {
+	      oldAddEventListener.call(this, event, client.wrap(callback), bubble);
+	    };
+	
+	    var oldRemoveEventListener = prototype.removeEventListener;
+	    prototype.removeEventListener = function(event, callback, bubble) {
+	      oldRemoveEventListener.call(this, event, callback && callback._wrapped || callback, bubble);
+	    };
+	  }
+	}
+	
+	// Add an init() method to do the same things that the shim would do
+	var wrapper = {};
+	wrapper.init = function(config) {
+	  var notifier = new Notifier();
+	  notifier.configure(config); 
+	
+	  if (config.captureUncaught) {
+	    // Set the global onerror handler
+	    var old = window.onerror;
+	
+	    window.onerror = function() {
+	      var args = Array.prototype.slice.call(arguments, 0);
+	      _rollbarWindowOnError(notifier, old, args);
+	    };
+	
+	    // Adapted from https://github.com/bugsnag/bugsnag-js
+	    var globals = ['EventTarget', 'Window', 'Node', 'ApplicationCache', 'AudioTrackList', 'ChannelMergerNode', 'CryptoOperation', 'EventSource',
+	     'FileReader', 'HTMLUnknownElement', 'IDBDatabase', 'IDBRequest', 'IDBTransaction', 'KeyOperation', 'MediaController',
+	     'MessagePort', 'ModalWindow', 'Notification', 'SVGElementInstance', 'Screen', 'TextTrack', 'TextTrackCue',
+	     'TextTrackList', 'WebSocket', 'WebSocketWorker', 'Worker', 'XMLHttpRequest', 'XMLHttpRequestEventTarget',
+	     'XMLHttpRequestUpload'];
+	
+	    var i;
+	    var global;
+	    for (i = 0; i < globals.length; ++i) {
+	      global = globals[i];
+	
+	      if (window[global] && window[global].prototype) {
+	        _extendListenerPrototype(notifier, window[global].prototype);
+	      }
+	    }
+	  }
+	
+	  // Finally, start processing payloads using the global notifier
+	  Notifier.processPayloads();
+	  return notifier;
+	};
+	
+	
+	module.exports = {
+	  wrapper: wrapper,
+	  setupJSON: setupJSON
+	};
+
+
+/***/ },
 /* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
@@ -1378,4 +1451,4 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ ])
 });
 ;
-//# sourceMappingURL=rollbar.nojson.js.map
+//# sourceMappingURL=rollbar.umd.nojson.js.map
