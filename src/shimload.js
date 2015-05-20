b0b354c53a4ffa81d889f488c0b733e06b5a7c2b
Jon de Andres <jon@wuaki.tv>
2015-05-20 12:54:59 +0200

    Remove shimload.js.

--- src/shimload.js
+++
@@ -1,4 +0,0 @@
-var defaultRollbarJsUrl = '//d37gvrvc0wt4s1.cloudfront.net/js/v1.2/rollbar.min.js';
-_rollbarConfig.rollbarJsUrl = _rollbarConfig.rollbarJsUrl || defaultRollbarJsUrl;
-var r = Rollbar.init(window, _rollbarConfig);
-r.loadFull(window, document, false, _rollbarConfig);
