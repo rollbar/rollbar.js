b680210d7af548538c79f206b12ff3d5461fe658
Cory Virok <coryvirok@gmail.com>
2015-09-01 23:48:35 -0700

    Fixed package.json dev requirements

--- sdks/rollbar.js/dist/rollbar.umd.js
+++ sdks/rollbar.js/dist/rollbar.umd.js
@@ -2,7 +2,7 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define(factory);
+		define([], factory);
 	else {
 		var a = factory();
 		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
