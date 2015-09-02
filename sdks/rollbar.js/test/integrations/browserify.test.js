1c8eb42ad876ad3644aa6966878cdac4b303da3a
Cory Virok <coryvirok@gmail.com>
2015-09-02 12:21:04 -0700

    Add/fix Browserify tests

---
+++ sdks/rollbar.js/test/integrations/browserify.test.js
@@ -0,0 +1,16 @@
+var expect = chai.expect;
+
+describe('Include Rollbar via browserify', function() {
+  it('should load a valid Rollbar notifier', function(done) {
+    var rollbar = require('../../dist/rollbar.umd.nojson.min.js');
+    window.Rollbar = rollbar.init({});
+    expect(window.Rollbar).to.be.an('Object');
+    expect(window.Rollbar).to.have.property('configure');
+
+    window.Rollbar.error('error from Browserify test');
+    expect(window._rollbarPayloadQueue.length).to.equal(1);
+    expect(JSON.stringify(window._rollbarPayloadQueue[0])).to.contain('Browserify test');
+
+    done();
+  });
+});
