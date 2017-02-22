fc6a4e711d2ffe61fff3626897914d18ed0bff4a
Andrew Ledvina <wvvwwvw@gmail.com>
2017-02-22 14:54:15 -0800

    change browserify test to use different build

--- test/browserify.test.js
+++ test/browserify.test.js
@@ -1,6 +1,6 @@
 describe('Include Rollbar via browserify', function() {
   it('should load a valid Rollbar notifier', function(done) {
-    var rollbar = require('../dist/rollbar.umd.nojson.min.js');
+    var rollbar = require('../dist/rollbar.umd.min.js');
     window.Rollbar = rollbar.init({});
     expect(window.Rollbar).to.be.an('object');
     expect(window.Rollbar).to.have.property('configure');
