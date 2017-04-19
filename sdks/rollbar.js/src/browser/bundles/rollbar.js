6289955974776adf5b1b1ce4f6cfde2833308709
Andrew Ledvina <wvvwwvw@gmail.com>
2017-04-19 15:04:27 -0700

    refactor how captureUncaught and captureUnhandledRejections work so that umd stuff works right

--- sdks/rollbar.js/src/browser/bundles/rollbar.js
+++ sdks/rollbar.js/src/browser/bundles/rollbar.js
@@ -1,23 +1,16 @@
 var rollbar = require('../rollbar');
 var globals = require('../globalSetup');
 
-var options = window._rollbarConfig;
+var options = window && window._rollbarConfig;
 var alias = options && options.globalAlias || 'Rollbar';
-var shimRunning = window[alias] && typeof window[alias].shimId !== 'undefined';
+var shimRunning = window && window[alias] && typeof window[alias].shimId !== 'undefined';
 
-if (!window._rollbarStartTime) {
+if (window && !window._rollbarStartTime) {
   window._rollbarStartTime = (new Date()).getTime();
 }
 
 if (!shimRunning && options) {
   var Rollbar = new rollbar(options);
-  if (options.captureUncaught) {
-    globals.captureUncaughtExceptions(window, Rollbar);
-    globals.wrapGlobals(window, Rollbar);
-  }
-  if (options.captureUnhandledRejections) {
-    globals.captureUnhandledRejections(window, Rollbar);
-  }
   window[alias] = Rollbar;
 } else {
   window.rollbar = rollbar;
