ba1701b949003705f516f124de918d5a5743cb88
Andrew Ledvina <wvvwwvw@gmail.com>
2017-05-02 14:15:08 -0700

    bump to .8 and build

--- dist/rollbar.js
+++ dist/rollbar.js
@@ -352,7 +352,7 @@
 	/* global __DEFAULT_ENDPOINT__:false */
 	
 	var defaultOptions = {
-	  version: ("2.0.0-alpha.7"),
+	  version: ("2.0.0-alpha.8"),
 	  scrubFields: (["pw","pass","passwd","password","secret","confirm_password","confirmPassword","password_confirmation","passwordConfirmation","access_token","accessToken","secret_key","secretKey","secretToken"]),
 	  logLevel: ("debug"),
 	  reportLevel: ("debug"),
