e3ab9a3a059aeec42caa1e776d089b03f3c08b7e
Andrew Ledvina <wvvwwvw@gmail.com>
2017-05-31 16:50:53 -0700

    write docs on no-conflict, fix the global handlers to allow for multiple rollbar instances, bump the version number and build

--- sdks/rollbar.js/src/browser/globalSetup.js
+++ sdks/rollbar.js/src/browser/globalSetup.js
@@ -1,10 +1,10 @@
-function captureUncaughtExceptions(window, handler) {
+function captureUncaughtExceptions(window, handler, shim) {
   if (!window) { return; }
   var oldOnError;
 
   if (typeof handler._rollbarOldOnError === 'function') {
     oldOnError = handler._rollbarOldOnError;
-  } else if (window.onerror && !window.onerror.belongsToRollbar) {
+  } else if (window.onerror && !window.onerror.belongsToShim) {
     oldOnError = window.onerror;
     handler._rollbarOldOnError = oldOnError;
   }
@@ -13,7 +13,7 @@ function captureUncaughtExceptions(window, handler) {
     var args = Array.prototype.slice.call(arguments, 0);
     _rollbarWindowOnError(window, handler, oldOnError, args);
   };
-  fn.belongsToRollbar = true;
+  fn.belongsToShim = shim;
   window.onerror = fn;
 }
 
@@ -34,10 +34,10 @@ function _rollbarWindowOnError(window, r, old, args) {
   }
 }
 
-function captureUnhandledRejections(window, handler) {
+function captureUnhandledRejections(window, handler, shim) {
   if (!window) { return; }
 
-  if (typeof window._rollbarURH === 'function') {
+  if (typeof window._rollbarURH === 'function' && window._rollbaarURH.belongsToShim) {
     window.removeEventListener('unhandledrejection', window._rollbarURH);
   }
 
@@ -55,11 +55,12 @@ function captureUnhandledRejections(window, handler) {
       handler.handleUnhandledRejection(reason, promise);
     }
   };
+  rejectionHandler.belongsToShim = shim;
   window._rollbarURH = rejectionHandler;
   window.addEventListener('unhandledrejection', rejectionHandler);
 }
 
-function wrapGlobals(window, handler) {
+function wrapGlobals(window, handler, shim) {
   if (!window) { return; }
   // Adapted from https://github.com/bugsnag/bugsnag-js
   var globals = 'EventTarget,Window,Node,ApplicationCache,AudioTrackList,ChannelMergerNode,CryptoOperation,EventSource,FileReader,HTMLUnknownElement,IDBDatabase,IDBRequest,IDBTransaction,KeyOperation,MediaController,MessagePort,ModalWindow,Notification,SVGElementInstance,Screen,TextTrack,TextTrackCue,TextTrackList,WebSocket,WebSocketWorker,Worker,XMLHttpRequest,XMLHttpRequestEventTarget,XMLHttpRequestUpload'.split(',');
@@ -68,31 +69,33 @@ function wrapGlobals(window, handler) {
     global = globals[i];
 
     if (window[global] && window[global].prototype) {
-      _extendListenerPrototype(handler, window[global].prototype);
+      _extendListenerPrototype(handler, window[global].prototype, shim);
     }
   }
 }
 
-function _extendListenerPrototype(handler, prototype) {
+function _extendListenerPrototype(handler, prototype, shim) {
   if (prototype.hasOwnProperty && prototype.hasOwnProperty('addEventListener')) {
     var oldAddEventListener = prototype.addEventListener;
-    if (oldAddEventListener._rollbarOldAdd) {
+    while (oldAddEventListener._rollbarOldAdd && oldAddEventListener.belongsToShim) {
       oldAddEventListener = oldAddEventListener._rollbarOldAdd;
     }
     var addFn = function(event, callback, bubble) {
       oldAddEventListener.call(this, event, handler.wrap(callback), bubble);
     };
     addFn._rollbarOldAdd = oldAddEventListener;
+    addFn.belongsToShim = shim;
     prototype.addEventListener = addFn;
 
     var oldRemoveEventListener = prototype.removeEventListener;
-    if (oldRemoveEventListener._rollbarOldRemove) {
+    while (oldRemoveEventListener._rollbarOldRemove && oldRemoveEventListener.belongsToShim) {
       oldRemoveEventListener = oldRemoveEventListener._rollbarOldRemove;
     }
     var removeFn = function(event, callback, bubble) {
       oldRemoveEventListener.call(this, event, callback && callback._wrapped || callback, bubble);
     };
     removeFn._rollbarOldRemove = oldRemoveEventListener;
+    removeFn.belongsToShim = shim;
     prototype.removeEventListener = removeFn;
   }
 }
