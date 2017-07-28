9583df079ae3324b93fd490c29ab7bfc044b3e6d
Andrew Ledvina <wvvwwvw@gmail.com>
2017-07-28 09:51:14 +0200

    allow for uuids in each capture method, change connectivity to a network event with a different subtype, other comments, use network status to get level

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,76 +1,91 @@
 var _ = require('./utility');
 
+var MAX_EVENTS = 100;
+
 function Telemeter(options) {
   this.queue = [];
   this.options = _.extend(true, {}, options);
-  var maxTelemetryEvents = this.options.maxTelemetryEvents;
-  // pin the queue size to the interval [0, 100]
-  this.maxQueueSize = Math.max(0, Math.min(maxTelemetryEvents || 100, 100))
+  var maxTelemetryEvents = this.options.maxTelemetryEvents || MAX_EVENTS;
+  this.maxQueueSize = Math.max(0, Math.min(maxTelemetryEvents, MAX_EVENTS));
 }
 
 Telemeter.prototype.copyEvents = function() {
   return Array.prototype.slice.call(this.queue, 0);
 };
 
-Telemeter.prototype.capture = function(type, metadata, level) {
+Telemeter.prototype.capture = function(type, metadata, level, rollbarUUID, timestamp) {
   var e = {
     level: getLevel(type, level),
     type: type,
-    timestamp_ms: _.now(),
+    timestamp_ms: timestamp || _.now(),
     body: metadata
   };
+  if (rollbarUUID) {
+    e.uuid = rollbarUUID;
+  }
   this.push(e);
   return e;
 };
 
-Telemeter.prototype.captureEvent = function(metadata, level) {
-  return this.capture('manual', metadata, level);
-}
-
-Telemeter.prototype.captureError = function(err) {
-  return this.capture('error', {
-    message: err.message || String(err)
-  }, 'error');
+Telemeter.prototype.captureEvent = function(metadata, level, rollbarUUID) {
+  return this.capture('manual', metadata, level, rollbarUUID);
 };
 
-Telemeter.prototype.captureLog = function(message, level) {
+Telemeter.prototype.captureError = function(err, level, rollbarUUID, timestamp) {
+  var metadata = {
+    message: err.message || String(err)
+  };
+  if (err.stack) {
+    metadata.stack = err.stack;
+  }
+  return this.capture('error', metadata, level, rollbarUUID, timestamp);
+};
+
+Telemeter.prototype.captureLog = function(message, level, rollbarUUID, timestamp) {
   return this.capture('log', {
     message: message
-  }, level);
+  }, level, rollbarUUID, timestamp);
 };
 
-Telemeter.prototype.captureNetwork = function(metadata, subtype) {
+Telemeter.prototype.captureNetwork = function(metadata, subtype, status, rollbarUUID) {
   subtype = subtype || 'xhr';
   metadata.subtype = metadata.subtype || subtype;
-  return this.capture('network', metadata, 'info');
+  var level = levelFromStatus(status);
+  return this.capture('network', metadata, level, rollbarUUID);
 };
 
-Telemeter.prototype.captureRollbar = function(item) {
-  return this.capture('rollbar', {
-    uuid: item.uuid
-  }, item.level);
+Telemeter.prototype.captureItem = function(item) {
+  if (item.err) {
+    return this.captureError(item.err, item.level, item.uuid, item.timestamp);
+  }
+  if (item.message) {
+    return this.captureLog(item.message, item.level, item.uuid, item.timestamp);
+  }
+  if (item.custom) {
+    return this.capture('log', item.custom, item.level, item.uuid, item.timestamp);
+  }
 };
 
-Telemeter.prototype.captureDom = function(subtype, element, value, checked) {
+Telemeter.prototype.captureDom = function(subtype, element, value, checked, rollbarUUID) {
   var metadata = {
     subtype: subtype,
     element: element
   };
   if (value !== undefined) {
-    metadata['value'] = value;
+    metadata.value = value;
   }
   if (checked !== undefined) {
-    metadata['checked'] = checked;
+    metadata.checked = checked;
   }
-  return this.capture('dom', metadata, 'info');
+  return this.capture('dom', metadata, 'info', rollbarUUID);
 };
 
-Telemeter.prototype.captureNavigation = function(from, to) {
-  return this.capture('navigation', {from: from, to: to}, 'info');
+Telemeter.prototype.captureNavigation = function(from, to, rollbarUUID) {
+  return this.capture('navigation', {from: from, to: to}, 'info', rollbarUUID);
 };
 
-Telemeter.prototype.captureConnectivityChange = function(type) {
-  return this.capture('connectivity', {change: type}, 'info');
+Telemeter.prototype.captureConnectivityChange = function(type, rollbarUUID) {
+  return this.captureNetwork({change: type}, 'connectivity', undefined, rollbarUUID);
 };
 
 Telemeter.prototype.push = function(e) {
@@ -85,10 +100,20 @@ function getLevel(type, level) {
     return level;
   }
   var defaultLevel = {
-    'error': 'error',
-    'manual': 'info'
+    error: 'error',
+    manual: 'info'
   };
   return defaultLevel[type] || 'info';
+}
+
+function levelFromStatus(status) {
+  if (status >= 200 && status < 400) {
+    return 'info';
+  }
+  if (status >= 400) {
+    return 'error';
+  }
+  return 'info';
 }
 
 module.exports = Telemeter;
