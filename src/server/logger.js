174251e957f1cc89865c2d4af93997f607155b2f
Guilherme Conti <guibasconti@gmail.com>
2017-10-24 00:40:45 -0200

    Disable no-console eslint

--- src/server/logger.js
+++ src/server/logger.js
@@ -3,8 +3,10 @@
 var name = 'Rollbar';
 
 var logger = {
+  /* eslint-disable no-console */
   log: console.log.bind(console),
   error: console.error.bind(console)
+  /* eslint-enable no-console */
 };
 
 module.exports = logger;
