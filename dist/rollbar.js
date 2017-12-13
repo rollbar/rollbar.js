ab287208e26870c842f4422fa6a16f019b214a13
Andrew Weiss <wvvwwvw@gmail.com>
2017-12-13 15:43:47 -0800

    Release v2.3.3

--- dist/rollbar.js
+++ dist/rollbar.js
@@ -462,7 +462,7 @@
 	/* global __DEFAULT_ENDPOINT__:false */
 	
 	var defaultOptions = {
-	  version: ("2.3.2"),
+	  version: ("2.3.3"),
 	  scrubFields: (["pw","pass","passwd","password","secret","confirm_password","confirmPassword","password_confirmation","passwordConfirmation","access_token","accessToken","secret_key","secretKey","secretToken"]),
 	  logLevel: ("debug"),
 	  reportLevel: ("debug"),
@@ -591,8 +591,8 @@
 	      delete item.callback;
 	    }
 	    item.level = item.level || defaultLevel;
-	    item.telemetryEvents = this.telemeter.copyEvents();
 	    this.telemeter._captureRollbarItem(item);
+	    item.telemetryEvents = this.telemeter.copyEvents();
 	    this.notifier.log(item, callback);
 	  } catch (e) {
 	    this.logger.error(e)
