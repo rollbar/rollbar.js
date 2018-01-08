35e5050b9f54d7aa5f70241639c35ad29513308b
Zach Collins <recursive.cookie.jar@gmail.com>
2018-01-08 15:38:00 -0800

    Remove rollbar specific nix options

--- shell.nix
+++ shell.nix
@@ -1,58 +1,12 @@
-let pkgs  = import <nixpkgs> {};
-    pkgs' = import pkgs.path { overlays = [ (import ../vagrant-dev-vm/nix) ]; };
-in pkgs'.callPackage ({
-  nodejs-6_x,
-  stdenv,
-  writeScriptBin,
-  bash,
-  devEnvs,
-  moxLib,
-  darwin,
-}:
+{ pkgs ? import <nixpkgs> {}, nodejs-6_x ? pkgs.nodejs-6_x, stdenv ? pkgs.stdenv }:
 
 let
 node = nodejs-6_x;
-config = moxLib.projectConfig ./. {
-  env = {
-  };
-};
 
 in
 
 stdenv.mkDerivation rec {
   name = "rollbar.js-env";
 
-  buildInputs = [
-    node darwin.apple_sdk.frameworks.CoreServices
-    moxUpdate
-  ];
-
-  projectDir = toString ./.;
-  hardeningDisable = [ "all" ];
-
-  moxUpdate = writeScriptBin "mox-update" ''
-    #!${bash}/bin/bash
-    set -x
-
-    cd ${projectDir}
-    unset NIX_ENFORCE_PURITY
-
-    git submodule update --init
-
-    npm install
-  '';
-
-  provision = devEnvs.ensureProvisioned ''
-    mox-update
-  '';
-
-  shellHook = ''
-    cd ${projectDir}
-
-    ${provision}
-
-    ${devEnvs.withNodeBin}
-  '';
-} // config.env
-
-) {}
+  buildInputs = [ node ];
+}
