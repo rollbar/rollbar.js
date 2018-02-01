463abc3bdeb1eb9746d414515fa92d9cc9967773
Andrew Weiss <wvvwwvw@gmail.com>
2018-02-01 14:42:50 -0800

    Proper global checks

--- sdks/rollbar.js/src/browser/bundles/rollbar.noconflict.js
+++ sdks/rollbar.js/src/browser/bundles/rollbar.noconflict.js
@@ -1,6 +1,6 @@
 var rollbar = require('../rollbar');
 
-if (window && !window._rollbarStartTime) {
+if ((typeof window !== 'undefined') && !window._rollbarStartTime) {
   window._rollbarStartTime = (new Date()).getTime();
 }
 
