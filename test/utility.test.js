d50272dd19893cc24fe22c2437f2d69d0516517f
Walt Jones <waltjones@gmail.com>
2020-06-30 09:27:55 -0700

    Allow scrubbing by full path in payload (#869)
    * feat: allow scrubbing by full path in payload
    
    * test: use redact() function in tests

--- test/utility.test.js
+++ test/utility.test.js
@@ -656,7 +656,7 @@ describe('scrub', function() {
 
     var result = _.scrub(data, scrubFields);
 
-    expect(result.password).to.not.eql('abc123');
+    expect(result.password).to.eql(_.redact());
   });
   it('should handle nested objects', function() {
     var data = {
@@ -675,10 +675,10 @@ describe('scrub', function() {
     var result = _.scrub(data, scrubFields);
 
     expect(result.a.b.other).to.eql('stuff');
-    expect(result.a.badthing).to.not.eql('secret');
+    expect(result.a.b.badthing).to.eql(_.redact());
     expect(result.a.c).to.eql('bork');
-    expect(result.a.password).to.not.eql('abc123');
-    expect(result.secret).to.not.eql('blahblah');
+    expect(result.a.password).to.eql(_.redact());
+    expect(result.secret).to.eql(_.redact());
     expect(data.secret).to.eql('blahblah');
   });
   it('should do something sane for recursive objects', function() {
@@ -697,10 +697,37 @@ describe('scrub', function() {
     var result = _.scrub(data, scrubFields);
 
     expect(result.thing).to.eql('stuff');
-    expect(result.password).to.not.eql('abc123');
-    expect(result.inner.a).to.not.eql('what');
+    expect(result.password).to.eql(_.redact());
+    expect(result.inner.a).to.eql(_.redact());
     expect(result.inner.a).to.be.ok();
     expect(result.inner.b).to.eql('yes');
+  });
+  it('should handle scrubPaths', function() {
+    var data = {
+      a: {
+        b: {
+          foo: 'secret',
+          bar: 'stuff'
+        },
+        c: 'bork',
+        password: 'abc123'
+      },
+      secret: 'blahblah'
+    };
+    var scrubPaths = [
+      'nowhere', // path not found
+      'a.b.foo', // nested path
+      'a.password', // nested path
+      'secret' // root path
+    ];
+
+    var result = _.scrub(data, [], scrubPaths);
+
+    expect(result.a.b.bar).to.eql('stuff');
+    expect(result.a.b.foo).to.eql(_.redact());
+    expect(result.a.c).to.eql('bork');
+    expect(result.a.password).to.eql(_.redact());
+    expect(result.secret).to.eql(_.redact());
   });
 });
 
