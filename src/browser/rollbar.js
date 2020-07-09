0dae97b33b1ce8508e93282d30f676d43b054ca1
Walt Jones <waltjones@gmail.com>
2020-07-09 15:58:00 -0700

    feat: enable componentized scrubber

--- src/browser/rollbar.js
+++ src/browser/rollbar.js
@@ -3,12 +3,14 @@ var telemeter = require('../telemetry');
 var instrumenter = require('./telemetry');
 var polyfillJSON = require('../../vendor/JSON-js/json3');
 var wrapGlobals = require('./wrapGlobals');
+var scrub = require('../scrub');
 
 Rollbar.setComponents({
   telemeter: telemeter,
   instrumenter: instrumenter,
   polyfillJSON: polyfillJSON,
-  wrapGlobals: wrapGlobals
+  wrapGlobals: wrapGlobals,
+  scrub: scrub
 });
 
 module.exports = Rollbar;
---
+++ src/utility/traverse.js
@@ -0,0 +1,38 @@
+var _ = require('../utility');
+
+function traverse(obj, func, seen) {
+  var k, v, i;
+  var isObj = _.isType(obj, 'object');
+  var isArray = _.isType(obj, 'array');
+  var keys = [];
+
+  if (isObj && seen.indexOf(obj) !== -1) {
+    return obj;
+  }
+  seen.push(obj);
+
+  if (isObj) {
+    for (k in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, k)) {
+        keys.push(k);
+      }
+    }
+  } else if (isArray) {
+    for (i = 0; i < obj.length; ++i) {
+      keys.push(i);
+    }
+  }
+
+  var result = isObj ? {} : [];
+  var same = true;
+  for (i = 0; i < keys.length; ++i) {
+    k = keys[i];
+    v = obj[k];
+    result[k] = func(k, v, seen);
+    same = same && result[k] === obj[k];
+  }
+
+  return (keys.length != 0 && !same) ? result : obj;
+}
+
+module.exports = traverse;
