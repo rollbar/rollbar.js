71d8feec4f3af7bf0dba50f2a6c2ecac2627476b
Walt Jones <waltjones@gmail.com>
2020-10-02 12:41:19 -0400

    fix: add ts types for componentized package (#892)

---
+++ sdks/rollbar.js/src/telemetry.d.ts
@@ -0,0 +1,5 @@
+import Rollbar from '../index';
+
+declare var Telemeter: Rollbar.TelemeterType;
+
+export = Telemeter;
---
+++ sdks/rollbar.js/src/truncation.d.ts
@@ -0,0 +1,5 @@
+import Rollbar from '../index';
+
+declare var Truncation: Rollbar.TruncationType;
+
+export = Truncation;
