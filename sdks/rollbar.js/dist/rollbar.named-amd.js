444138d928df16489a3f3ac2d82778feecc3fec8
Walt Jones <waltjones@gmail.com>
2021-07-01 05:06:06 -0700

    Release v2.24.0 (#960)

--- sdks/rollbar.js/dist/rollbar.named-amd.js
+++ sdks/rollbar.js/dist/rollbar.named-amd.js
@@ -4553,7 +4553,7 @@ module.exports = {
 
 
 module.exports = {
-  version: '2.23.0',
+  version: '2.24.0',
   endpoint: 'api.rollbar.com/api/1/item/',
   logLevel: 'debug',
   reportLevel: 'debug',
