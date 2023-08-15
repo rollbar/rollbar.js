2514fdf5ea66adb5ddb7efe0fc71933828c865d5
Ilya Kuznetsov <ilia.kuznetsov@fingerprint.com>
2023-08-15 13:25:29 +0400

    test: Fetch telemetry tests

--- test/browser.telemetry.test.js
+++ test/browser.telemetry.test.js
@@ -8,17 +8,17 @@ var Instrumenter = require('../src/browser/telemetry');
 describe('instrumentNetwork', function () {
   it('should capture XHR requests with string URL', function (done) {
     var callback = sinon.spy();
-    var mockWindow = {
+    var windowMock = {
       XMLHttpRequest: function () { }
     }
 
-    mockWindow.XMLHttpRequest.prototype.open = function () { }
-    mockWindow.XMLHttpRequest.prototype.send = function () { }
+    windowMock.XMLHttpRequest.prototype.open = function () { }
+    windowMock.XMLHttpRequest.prototype.send = function () { }
 
-    var i = new Instrumenter({ scrubFields: [] }, { captureNetwork: callback }, { wrap: function () { }, client: { notifier: { diagnostic: {} } } }, mockWindow);
+    var i = createInstrumenter(callback, windowMock)
     i.instrumentNetwork()
 
-    var xhr = new mockWindow.XMLHttpRequest();
+    var xhr = new windowMock.XMLHttpRequest();
     xhr.open('GET', 'http://first.call')
     xhr.send()
     xhr.onreadystatechange()
@@ -27,9 +27,9 @@ describe('instrumentNetwork', function () {
     expect(callback.args[0][0].url).to.eql('http://first.call')
 
     i.deinstrumentNetwork()
+    i = createInstrumenter(callback, windowMock)
     i.instrumentNetwork()
-
-    var xhr = new mockWindow.XMLHttpRequest();
+    var xhr = new windowMock.XMLHttpRequest();
     xhr.open('GET', new URL('http://second.call'))
     xhr.send()
     xhr.onreadystatechange()
@@ -38,4 +38,32 @@ describe('instrumentNetwork', function () {
 
     done()
   })
+
+  it('should capture XHR requests with string URL', function (done) {
+    var callback = sinon.spy();
+    var windowMock = {
+      fetch: function () { return Promise.resolve() }
+    }
+
+    var i = createInstrumenter(callback, windowMock);
+    i.instrumentNetwork()
+
+    windowMock.fetch('http://first.call')
+    expect(callback.callCount).to.eql(1)
+    expect(callback.args[0][0].url).to.eql('http://first.call')
+
+    i.deinstrumentNetwork()
+    i = createInstrumenter(callback, windowMock)
+    i.instrumentNetwork()
+
+    windowMock.fetch(new URL('http://second.call'))
+    expect(callback.callCount).to.eql(2)
+    expect(callback.args[1][0].url).to.eql('http://second.call/')
+
+    done()
+  })
 })
+
+function createInstrumenter(callback, windowMock) {
+  return new Instrumenter({ scrubFields: [] }, { captureNetwork: callback }, { wrap: function () { }, client: { notifier: { diagnostic: {} } } }, windowMock);
+}
