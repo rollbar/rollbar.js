1aeb7df3caecbac4d341e629a227395668ebb0f0
Matias Pequeno <matias.pequeno@gmail.com>
2025-07-22 16:18:01 -0300

    Use concatenation to build the cdn url for the browser snippet

--- src/browser/bundles/defaults.js
+++ src/browser/bundles/defaults.js
@@ -1,4 +1,5 @@
 import { version } from '../../defaults.js'
 
 export const cdnHost = 'cdn.rollbar.com';
-export const defaultRollbarJsUrl = `https://${cdnHost}/rollbarjs/refs/tags/v${version}/rollbar.min.js`;
+// concatenation over interpolation makes the minified file smaller
+export const defaultRollbarJsUrl = 'https://' + cdnHost + '/rollbarjs/refs/tags/v' + version + '/rollbar.min.js';
