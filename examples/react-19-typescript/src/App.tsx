ae61f09f6af9f7a662b86a1953ceca92c76a3246
Matias Pequeno <matias.pequeno@gmail.com>
2025-07-31 16:27:59 -0300

    Added React 19 TS example (#1270)

---
+++ examples/react-19-typescript/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Rollbar from 'rollbar';
+const instance: Rollbar = new Rollbar(); // TODO(matux): finish example once import is working
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
