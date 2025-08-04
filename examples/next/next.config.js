e6fe778849485e372464519d915bb8a1f303b133
Matias Pequeno <matias.pequeno@gmail.com>
2025-08-04 19:10:44 -0300

    removed yarn stuff

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -4,12 +4,6 @@ module.exports = {
   webpack(config, { isServer }) {
     config.devtool = "source-map";
     config.optimization.minimize = false;
-
-    config.resolve.alias.rollbar = path.resolve(
-      __dirname,
-      "../../rollbar/rollbar-js"
-    );
-
     return config;
   },
 };
