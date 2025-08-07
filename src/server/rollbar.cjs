484732d942ec81aab08bbba6ce9dc3e209740ce3
Matias Pequeno <matias.pequeno@gmail.com>
2025-08-07 18:36:19 -0300

    Do a commonjs dist file for node, when using require plus example (#1287)

--- src/server/rollbar.cjs
+++
@@ -1,5 +0,0 @@
-// CommonJS wrapper for ES module
-// This file provides CommonJS compatibility for Node.js users
-// who require() the rollbar package
-
-module.exports = require('./rollbar.js').default;
