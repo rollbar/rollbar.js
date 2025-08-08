8c1398d3e2acfd90560a2d511000da63651cde35
Matias Pequeno <matias.pequeno@gmail.com>
2025-08-08 10:01:01 -0300

    Release 3.0.0-alpha.6

--- src/defaults.js
+++ src/defaults.js
@@ -1,7 +1,7 @@
 /**
  * Default options shared across platforms
  */
-export const version = '3.0.0-alpha.5';
+export const version = '3.0.0-alpha.6';
 export const endpoint = 'api.rollbar.com/api/1/item/';
 export const logLevel = 'debug';
 export const reportLevel = 'debug';
