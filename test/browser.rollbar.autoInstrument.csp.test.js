9daf2b988b1c29a37e4b000df0a2ba1f60c539b9
Matias Pequeno <matias.pequeno@gmail.com>
2025-09-08 15:54:13 -0300

    fixed all remaining browser tests

---
+++ test/browser.rollbar.autoInstrument.csp.test.js
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { setTimeout } from './util/timers.js';
+
+import Rollbar from '../src/browser/rollbar.js';
+
+describe('options.autoInstrument', function () {
+  describe('contentSecurityPolicy', function () {
+    let rollbar = null;
+
+    beforeEach(function () {
+      rollbar = window.rollbar = new Rollbar({
+        accessToken: 'POST_CLIENT_ITEM_TOKEN',
+        autoInstrument: {
+          log: false,
+          contentSecurityPolicy: true,
+          errorOnContentSecurityPolicy: true,
+        },
+      });
+    });
+
+    afterEach(function () {
+      window.rollbar.configure({
+        autoInstrument: false,
+        captureUncaught: false,
+      });
+    });
+
+    it('should report content security policy errors', async function () {
+      const queue = rollbar.client.notifier.queue;
+      const queueStub = sinon.stub(queue, '_makeApiRequest');
+
+      // Manually trigger a CSP violation event,
+      // since WTR doesn't set CSP headers, we can't use loadHtml to do it.
+      const cspEvent = new SecurityPolicyViolationEvent(
+        'securitypolicyviolation',
+        {
+          blockedURI: 'https://example.com/v3/',
+          violatedDirective: 'script-src',
+          effectiveDirective: 'script-src',
+          originalPolicy: "default-src 'self' 'unsafe-inline' 'unsafe-eval';",
+          sourceFile: window.location.href,
+          lineNumber: 1,
+          columnNumber: 1,
+        },
+      );
+
+      document.dispatchEvent(cspEvent);
+
+      await setTimeout(100);
+
+      expect(queueStub.called).to.be.true;
+      const item = queueStub.getCall(0).args[0];
+      const message = item.body.message.body;
+      const telemetry = item.body.telemetry[0];
+
+      expect(message).to.match(/Security Policy Violation/);
+      expect(message).to.match(/blockedURI: https:\/\/example.com\/v3\//);
+      expect(message).to.match(/violatedDirective: script-src/);
+      expect(message).to.match(
+        /originalPolicy: default-src 'self' 'unsafe-inline' 'unsafe-eval';/,
+      );
+
+      expect(telemetry.level).to.eql('error');
+      expect(telemetry.type).to.eql('log');
+      expect(telemetry.body.message).to.match(/Security Policy Violation/);
+      expect(telemetry.body.message).to.match(
+        /blockedURI: https:\/\/example.com\/v3\//,
+      );
+      expect(telemetry.body.message).to.match(/violatedDirective: script-src/);
+      expect(telemetry.body.message).to.match(
+        /originalPolicy: default-src 'self' 'unsafe-inline' 'unsafe-eval';/,
+      );
+    });
+  });
+});
