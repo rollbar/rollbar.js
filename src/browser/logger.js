1b756221ad13ae4293602531d2bb9702c1cfdcd3
Walt Jones <walt.jones@rollbar.com>
2025-09-22 17:17:37 -0400

    drop console polyfill and IEx code from logger (#1322)

--- src/browser/logger.js
+++ src/browser/logger.js
@@ -1,35 +1,21 @@
-import 'console-polyfill';
-import detection from './detection.js';
 import * as _ from '../utility.js';
 
 function error() {
   var args = Array.prototype.slice.call(arguments, 0);
   args.unshift('Rollbar:');
-  if (detection.ieVersion() <= 8) {
-    console.error(_.formatArgsAsString(args));
-  } else {
-    console.error.apply(console, args);
-  }
+  console.error.apply(console, args);
 }
 
 function info() {
   var args = Array.prototype.slice.call(arguments, 0);
   args.unshift('Rollbar:');
-  if (detection.ieVersion() <= 8) {
-    console.info(_.formatArgsAsString(args));
-  } else {
-    console.info.apply(console, args);
-  }
+  console.info.apply(console, args);
 }
 
 function log() {
   var args = Array.prototype.slice.call(arguments, 0);
   args.unshift('Rollbar:');
-  if (detection.ieVersion() <= 8) {
-    console.log(_.formatArgsAsString(args));
-  } else {
-    console.log.apply(console, args);
-  }
+  console.log.apply(console, args);
 }
 
 export default {
