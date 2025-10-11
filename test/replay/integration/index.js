68e6c572f8eb68e8643f7a0d0249633325d13fef
Matias Pequeno <matias.pequeno@gmail.com>
2025-10-11 12:19:28 -0300

    Remove index.js from tests (#1366)

--- test/replay/integration/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Session Replay Integration Tests
- */
-
-export * from './sessionRecording.test.js';
-export * from './api.spans.test.js';
-export * from './replayManager.test.js';
-export * from './queue.replayManager.test.js';
-export * from './e2e.test.js';
