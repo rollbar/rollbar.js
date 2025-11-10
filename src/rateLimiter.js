ffd241514699e8f9008b1f341bd7260f6c1ed6fe
Matias Pequeno <matias.pequeno@gmail.com>
2025-11-10 11:08:04 -0300

    Turn RateLimiter into a class

--- src/rateLimiter.js
+++ src/rateLimiter.js
@@ -1,108 +1,114 @@
-import * as _ from './utility.js';
-
 /*
- * RateLimiter - an object that encapsulates the logic for counting items sent to Rollbar
+ * RateLimiter - encapsulates the logic for counting items sent to Rollbar.
  *
  * @param options - the same options that are accepted by configureGlobal offered as a convenience
  */
-function RateLimiter(options) {
-  this.startTime = _.now();
-  this.counter = 0;
-  this.perMinCounter = 0;
-  this.platform = null;
-  this.platformOptions = {};
-  this.configureGlobal(options);
-}
+class RateLimiter {
+  static globalSettings = {
+    startTime: Date.now(),
+    maxItems: null,
+    itemsPerMinute: null,
+  };
 
-RateLimiter.globalSettings = {
-  startTime: _.now(),
-  maxItems: undefined,
-  itemsPerMinute: undefined,
-};
-
-/*
- * configureGlobal - set the global rate limiter options
- *
- * @param options - Only the following values are recognized:
- *    startTime: a timestamp of the form returned by (new Date()).getTime()
- *    maxItems: the maximum items
- *    itemsPerMinute: the max number of items to send in a given minute
- */
-RateLimiter.prototype.configureGlobal = function (options) {
-  if (options.startTime !== undefined) {
-    RateLimiter.globalSettings.startTime = options.startTime;
-  }
-  if (options.maxItems !== undefined) {
-    RateLimiter.globalSettings.maxItems = options.maxItems;
-  }
-  if (options.itemsPerMinute !== undefined) {
-    RateLimiter.globalSettings.itemsPerMinute = options.itemsPerMinute;
-  }
-};
-
-/*
- * shouldSend - determine if we should send a given item based on rate limit settings
- *
- * @param item - the item we are about to send
- * @returns An object with the following structure:
- *  error: (Error|null)
- *  shouldSend: bool
- *  payload: (Object|null)
- *  If shouldSend is false, the item passed as a parameter should not be sent to Rollbar, and
- *  exactly one of error or payload will be non-null. If error is non-null, the returned Error will
- *  describe the situation, but it means that we were already over a rate limit (either globally or
- *  per minute) when this item was checked. If error is null, and therefore payload is non-null, it
- *  means this item put us over the global rate limit and the payload should be sent to Rollbar in
- *  place of the passed in item.
- */
-RateLimiter.prototype.shouldSend = function (item, now) {
-  now = now || _.now();
-  var elapsedTime = now - this.startTime;
-  if (elapsedTime < 0 || elapsedTime >= 60000) {
-    this.startTime = now;
+  constructor(options = {}) {
+    this.startTime = Date.now();
+    this.counter = 0;
     this.perMinCounter = 0;
+    this.platform = null;
+    this.platformOptions = {};
+    this.configureGlobal(options);
   }
 
-  var globalRateLimit = RateLimiter.globalSettings.maxItems;
-  var globalRateLimitPerMin = RateLimiter.globalSettings.itemsPerMinute;
+  /*
+   * configureGlobal - set the global rate limiter options
+   *
+   * @param options - Only the following values are recognized:
+   *    startTime: a timestamp of the form returned by (new Date()).getTime()
+   *    maxItems: the maximum items
+   *    itemsPerMinute: the max number of items to send in a given minute
+   */
+  configureGlobal(options = {}) {
+    const { startTime, maxItems, itemsPerMinute } = options;
 
-  if (checkRate(item, globalRateLimit, this.counter)) {
+    if (startTime != null) {
+      RateLimiter.globalSettings.startTime = startTime;
+    }
+    if (maxItems != null) {
+      RateLimiter.globalSettings.maxItems = maxItems;
+    }
+    if (itemsPerMinute != null) {
+      RateLimiter.globalSettings.itemsPerMinute = itemsPerMinute;
+    }
+  }
+
+  /*
+   * shouldSend - determine if we should send a given item based on rate limit settings
+   *
+   * @param item - the item we are about to send
+   * @returns An object with the following structure:
+   *  error: (Error|null)
+   *  shouldSend: bool
+   *  payload: (Object|null)
+   *  If shouldSend is false, the item passed as a parameter should not be sent to Rollbar, and
+   *  exactly one of error or payload will be non-null. If error is non-null, the returned Error will
+   *  describe the situation, but it means that we were already over a rate limit (either globally or
+   *  per minute) when this item was checked. If error is null, and therefore payload is non-null, it
+   *  means this item put us over the global rate limit and the payload should be sent to Rollbar in
+   *  place of the passed in item.
+   */
+  shouldSend(item, now = Date.now()) {
+    const elapsedTime = now - this.startTime;
+    if (elapsedTime < 0 || elapsedTime >= 60000) {
+      this.startTime = now;
+      this.perMinCounter = 0;
+    }
+
+    const globalRateLimit = RateLimiter.globalSettings.maxItems;
+    const globalRateLimitPerMin = RateLimiter.globalSettings.itemsPerMinute;
+
+    if (checkRate(item, globalRateLimit, this.counter)) {
+      return shouldSendValue(
+        this.platform,
+        this.platformOptions,
+        `${globalRateLimit} max items reached`,
+        false,
+      );
+    }
+
+    if (checkRate(item, globalRateLimitPerMin, this.perMinCounter)) {
+      return shouldSendValue(
+        this.platform,
+        this.platformOptions,
+        `${globalRateLimitPerMin} items per minute reached`,
+        false,
+      );
+    }
+
+    this.counter += 1;
+    this.perMinCounter += 1;
+
+    const underGlobalLimit = !checkRate(item, globalRateLimit, this.counter);
+    const perMinute = underGlobalLimit;
+    const shouldSend =
+      underGlobalLimit &&
+      !checkRate(item, globalRateLimitPerMin, this.perMinCounter);
+
     return shouldSendValue(
       this.platform,
       this.platformOptions,
-      globalRateLimit + ' max items reached',
-      false,
-    );
-  } else if (checkRate(item, globalRateLimitPerMin, this.perMinCounter)) {
-    return shouldSendValue(
-      this.platform,
-      this.platformOptions,
-      globalRateLimitPerMin + ' items per minute reached',
-      false,
+      null,
+      shouldSend,
+      globalRateLimit,
+      globalRateLimitPerMin,
+      perMinute,
     );
   }
-  this.counter++;
-  this.perMinCounter++;
 
-  var shouldSend = !checkRate(item, globalRateLimit, this.counter);
-  var perMinute = shouldSend;
-  shouldSend =
-    shouldSend && !checkRate(item, globalRateLimitPerMin, this.perMinCounter);
-  return shouldSendValue(
-    this.platform,
-    this.platformOptions,
-    null,
-    shouldSend,
-    globalRateLimit,
-    globalRateLimitPerMin,
-    perMinute,
-  );
-};
-
-RateLimiter.prototype.setPlatformOptions = function (platform, options) {
-  this.platform = platform;
-  this.platformOptions = options;
-};
+  setPlatformOptions(platform, options) {
+    this.platform = platform;
+    this.platformOptions = options;
+  }
+}
 
 /* Helpers */
 
@@ -119,11 +125,10 @@ function shouldSendValue(
   limitPerMin,
   perMinute,
 ) {
-  var payload = null;
-  if (error) {
-    error = new Error(error);
-  }
-  if (!error && !shouldSend) {
+  let payload = null;
+  const errorResult = error ? new Error(error) : null;
+
+  if (!errorResult && !shouldSend) {
     payload = rateLimitPayload(
       platform,
       options,
@@ -132,7 +137,8 @@ function shouldSendValue(
       perMinute,
     );
   }
-  return { error: error, shouldSend: shouldSend, payload: payload };
+
+  return { error: errorResult, shouldSend, payload };
 }
 
 function rateLimitPayload(
@@ -142,15 +148,12 @@ function rateLimitPayload(
   limitPerMin,
   perMinute,
 ) {
-  var environment =
+  const environment =
     options.environment || (options.payload && options.payload.environment);
-  var msg;
-  if (perMinute) {
-    msg = 'item per minute limit reached, ignoring errors until timeout';
-  } else {
-    msg = 'maxItems has been hit, ignoring errors until reset.';
-  }
-  var item = {
+  const msg = perMinute
+    ? 'item per minute limit reached, ignoring errors until timeout'
+    : 'maxItems has been hit, ignoring errors until reset.';
+  const item = {
     body: {
       message: {
         body: msg,
