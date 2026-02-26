d1325cb513c9706cd10598295b9b87ff41e5b5c8
Walt Jones <walt.jones@rollbar.com>
2026-02-26 11:54:58 -0500

    lint fix

--- src/utility.js
+++ src/utility.js
@@ -156,7 +156,7 @@ function uuid4() {
   var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
     /[xy]/g,
     function (c) {
-      var r = (d + Math.random() * 16) % 16 | 0;
+      var r = ((d + Math.random() * 16) % 16) | 0;
       d = Math.floor(d / 16);
       return (c === 'x' ? r : (r & 0x7) | 0x8).toString(16);
     },
